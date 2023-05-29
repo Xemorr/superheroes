36ca0aa399e5515cb315d98d62e035756e9ba852
Sam (Xemor) <samuelhollis@outlook.com>
2023-05-29 13:04:30 +0100

    Bumped version to 3.12.1 Bumped ConfigurationData version to 1.19.3 Changed default config to more clearly show that reroll item is disabled

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 group = "me.xemor"
-version = "3.12.0"
+version = "3.12.1"
 description = "superheroes"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
@@ -22,7 +22,7 @@ repositories {
 
 dependencies {
     shadow("de.themoep:minedown-adventure:1.7.1-SNAPSHOT")
-    shadow("me.xemor:configurationdata:1.19.2-SNAPSHOT")
+    shadow("me.xemor:configurationdata:1.19.3-SNAPSHOT")
     shadow("org.bstats:bstats-bukkit:1.7")
     shadow("me.xemor:UserInterface:1.6.6-SNAPSHOT")
     shadow("dev.dbassett:skullcreator:3.0.1")
