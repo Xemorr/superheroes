21b46b4adfb764a3ff7bcc00f3dc18c5eb90cf32
Xemor <xemor@xemor.me>
2026-03-09 13:50:04 +0000

    Default to empty string in language settings

--- src/main/java/me/xemor/superheroes/language/ChatLanguageSettings.java
+++ src/main/java/me/xemor/superheroes/language/ChatLanguageSettings.java
@@ -23,42 +23,66 @@ public class ChatLanguageSettings {
     private String invalidRerollGroupMessage;
 
     public String getGainedHero() {
-        if (gainedHero == null) Superheroes.getInstance().getLogger().severe("GainedHero is not set in the config.yml");
+        if (gainedHero == null) {
+            Superheroes.getInstance().getLogger().severe("GainedHero is not set in the config.yml");
+            gainedHero = "";
+        }
         return gainedHero;
     }
 
     public String getNoPermission() {
-        if (noPermission == null) Superheroes.getInstance().getLogger().severe("noPermission is not set in the config.yml");
+        if (noPermission == null) {
+            Superheroes.getInstance().getLogger().severe("noPermission is not set in the config.yml");
+            noPermission = "";
+        }
         return noPermission;
     }
 
     public String getCurrentHero() {
-        if (currentHero == null) Superheroes.getInstance().getLogger().severe("currentHero is not set in the config.yml");
+        if (currentHero == null) {
+            Superheroes.getInstance().getLogger().severe("currentHero is not set in the config.yml");
+            currentHero = "";
+        }
         return currentHero;
     }
 
     public String getHeroCommandCooldown() {
-        if (heroCommandCooldown == null) Superheroes.getInstance().getLogger().severe("heroCommandCooldown is not set in the config.yml");
+        if (heroCommandCooldown == null) {
+            Superheroes.getInstance().getLogger().severe("heroCommandCooldown is not set in the config.yml");
+            heroCommandCooldown = "";
+        }
         return heroCommandCooldown;
     }
 
     public String getInvalidHeroMessage() {
-        if (invalidHeroMessage == null) Superheroes.getInstance().getLogger().severe("invalidHeroMessage is not set in the config.yml");
+        if (invalidHeroMessage == null) {
+            Superheroes.getInstance().getLogger().severe("invalidHeroMessage is not set in the config.yml");
+            invalidHeroMessage = "";
+        }
         return invalidHeroMessage;
     }
 
     public String getInvalidPlayerMessage() {
-        if (invalidPlayerMessage == null) Superheroes.getInstance().getLogger().severe("invalidPlayerMessage is not set in the config.yml");
+        if (invalidPlayerMessage == null) {
+            Superheroes.getInstance().getLogger().severe("invalidPlayerMessage is not set in the config.yml");
+            invalidPlayerMessage = "";
+        }
         return invalidPlayerMessage;
     }
 
     public String getInvalidCommandMessage() {
-        if (invalidCommandMessage == null) Superheroes.getInstance().getLogger().severe("invalidCommandMessage is not set in the config.yml");
+        if (invalidCommandMessage == null) {
+            Superheroes.getInstance().getLogger().severe("invalidCommandMessage is not set in the config.yml");
+            invalidCommandMessage = "";
+        }
         return invalidCommandMessage;
     }
 
     public String getInvalidRerollGroupMessage() {
-        if (invalidRerollGroupMessage == null) Superheroes.getInstance().getLogger().severe("invalidRerollGroupMessage is not set in the config.yml");
+        if (invalidRerollGroupMessage == null) {
+            Superheroes.getInstance().getLogger().severe("invalidRerollGroupMessage is not set in the config.yml");
+            invalidRerollGroupMessage = "";
+        }
         return invalidRerollGroupMessage;
     }
 }
